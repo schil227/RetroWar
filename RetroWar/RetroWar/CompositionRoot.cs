cc4142838d8e2b0e957fdf5b6863a1a337f02ff2
schil227 <schil227@morris.umn.edu>
2019-10-16 22:50:51 -0500

    Initial commit - MonoGame seems really, really cool.

---
+++ RetroWar/RetroWar/CompositionRoot.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroWar
+{
+    public static class CompositionRoot
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<MainGame>();
+        }
+    }
+}
---
+++ RetroWar/RetroWar/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace RetroWar
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            CompositionRoot.AddServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            using (var game = serviceProvider.GetService<MainGame>())
+                game.Run();
+        }
+    }
+}
